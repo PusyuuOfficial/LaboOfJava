ba068f084636a5b7993fda7175994aea459639e9
Pusyuu <121094209+PusyuuOfficial@users.noreply.github.com>
2023-07-14 18:58:45 +0900

    add of file

---
+++ output sample.java
@@ -0,0 +1,7 @@
+/*関数内のpublicとは外部からアクセス可能にするもの、staticデータ型であるクラス内の関数を利用可能にする、void戻り値の入る場所今回はvoidなので無効、main関数名、(Strong[] args)引数・String型の文字列を扱う配列*/
+/*system.out.printlnはsysyemクラスの中にある出力を行うためのオブジェクトがoutである（printstreamクラスから作られるオブジェクト）でprintlnは文字列を出力する関数lnがついているので改行機能付き*/
+class test {
+  public static void main (Strong[] args) {
+    system.out.println("hello");
+  }
+}
