e421527ff795d1b21c1738820db84067629008a1
プシューわんこ/ <121094209+PusyuuWanko@users.noreply.github.com>
2024-02-23 01:35:39 +0000

    修正版　コンストラクタは戻りは存在しないのでエラーになる

--- extendsOfOverload.java
+++ extendsOfOverload.java
@@ -1,27 +1,30 @@
 /*
 継承のオーバーロードではそもそもオーバーロードとは同じ関数名で引数が違えば同じ名前でも使えるという機能です。
-スーパーメソッドの引数にいれOldクラス内のoldCfunに渡すことで引数ありなしが切り替わる。
-*/
+スーパーメソッドの引数にいれOldクラス内のOldに渡すことで引数ありなしが切り替わる。
+注意点継承の際は関数名とクラス名を同じにしなければならない。
+オーバーライドの際は引数の個数や型は同じでなければなりません。
+しかし、引数の有無は異なっても構いません。これを、オーバーロード（overloading）と呼びます。
 
+コンストラクタは戻り地を持たないそうです。
+*/
 class Old {
-  public void oldCfun(int x) {
-    System.out.println("引数ありバージョン");
+  public Old(int x) {
+    System.out.println("引数ありバージョン" + x);
   }
-  public void oldCfun() {
+  public Old() {
     System.out.println("引数なしバージョン");
   }
 }
 
-class New {
-  super();//機能しない。。。本当はこれで行くはずちなみに引数にいれOldクラス内のoldCfunに渡すことで引数ありなしが切り替わる。
-  public void newCfun() {
+class New extends Old {
+  public New() {
+    super();//機能しない。。。本当はこれで行くはずちなみに引数にいれOldクラス内のoldCfunに渡すことで引数ありなしが切り替わる。
     System.out.println("サブクラス");
   }
 }
 
-public class extendsOfOverload {
-    public static void main(String[] args) {
-        New obj = new New();
-        obj.newCfun();
-    }
+class extendsOfOverload {
+  public static void main(String[] args) {
+    New obj = new New();
+  }
 }
